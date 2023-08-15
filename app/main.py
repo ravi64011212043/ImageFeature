c43d1947a04689db9e0705f6235f5042de9b56a9
ravi64011212043 <64011212043@msu.ac.th>
2023-08-15 22:36:25 +0700

    first commit

---
+++ app/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+import base64
+import numpy as np
+import cv2
+app = FastAPI()
+
+# @app.get('/')
+# def root():
+#     return {"message": "Rawiwan"}
+
+@app.post("/api/genhog")
+def HogDescriptor(pic: str):
+        
+        data_split_img = pic.split(',',1)
+        img_string = data_split_img[1]
+
+        decode_img_data = base64.b64decode(img_string)
+
+        decode_image = cv2.imdecode(np.frombuffer(decode_img_data,np.uint8),cv2.IMREAD_GRAYSCALE)
+
+        img_new = cv2.resize(decode_image, (128,128), cv2.INTER_AREA)
+        
+        win_size = img_new.shape
+        cell_size = (8,8)
+        block_size = (16,16)
+        block_stride = (8,8)
+        num_bins = 9
+
+        hog = cv2.HOGDescriptor(win_size,block_size,block_stride,cell_size,num_bins)
+
+        hog_descriptor = hog.compute(img_new)
+
+        hog_descriptor_list = hog_descriptor.tolist()
+       
+        return {"HOG": hog_descriptor_list}
+    
+
+    
